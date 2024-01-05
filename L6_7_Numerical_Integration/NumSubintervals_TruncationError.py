019f0178c185f2c4a970066e1d81a75e25a9a910
Justin Sciortino <123967053+JustinSciortino@users.noreply.github.com>
2024-01-05 14:47:02 -0500

    Delete L6_7_Numerical_Integration directory

--- L6_7_Numerical_Integration/NumSubintervals_TruncationError.py
+++
@@ -1,9 +0,0 @@
-from math import ceil
-absoluteTruncationError = 0.01
-numberOfSubintervals = 10
-abTruncLessThan = 0.00001
-order = 3
-
-m = numberOfSubintervals*((absoluteTruncationError/abTruncLessThan)**(1/order))
-mRounded = ceil(m)
-print("Any value above ", mRounded, "is an answer.")
